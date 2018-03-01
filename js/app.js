c5cf2ccf8e87b1d090f62e806f4927726cf16607
lauralm <mein_herz_ist_kalt@hotmail.com>
2018-03-01 14:06:47 -0600

    Subiendo cambios

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-Escribe tu código aqui----
+let gallery = document.getElementsByClassName('gallery');
+let elementsGallery = gallery[0]['childNodes'];
+let imagesArray = [];
+for (let i in elementsGallery) {
+  if (i % 2 == 1) {
+    imagesArray.push(elementsGallery[i]);
+  }
+}
+$('#gallery').empty();
+let templateString = '';
+for (let content in imagesArray) {
+  let altText = imagesArray[content].alt;
+  let srcText = imagesArray[content].currentSrc;
+  templateString +=  `<figure class="cardify-figure">
+                        <img src="${srcText}" alt="${altText}">
+                        <figcaption class="style-figcaption">${altText}</figcaption>
+                    </figure>`;
+  console.log(srcText);
+}
+let father = document.querySelector('#gallery');
+father.innerHTML = templateString;
